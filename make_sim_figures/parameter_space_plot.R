3c81021004f8b1d66dd94598840be25bfd2d830a
Maya Weissman <47795173+mweissman97@users.noreply.github.com>
2026-07-24 13:53:50 -0700

    Create parameter_space_plot.R

---
+++ make_sim_figures/parameter_space_plot.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(viridis)
+
+# A) beneficial selection coefficient (s_ben)
+
+# B) deleterious selection coefficient (s_del)
+
+# C) background mutation rate (mu)
+
+# D) recombination rate (rho)
+
+# E) dominance coefficient (h)
+
+# F) beneficial mutation rate (theta)
